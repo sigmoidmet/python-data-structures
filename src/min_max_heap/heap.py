a9a67d2e0dcf7c7478ef4cc9ca5e44dcea1aeff6
burndmg <dmgburn@gmail.com>
2024-03-06 23:00:45 +0100

    MinMaxHeap create from an array and removeAt

--- src/min_max_heap/heap.py
+++ src/min_max_heap/heap.py
@@ -18,16 +18,87 @@
 # A max-min heap is defined analogously;
 # in such a heap, the maximum value is stored at the root, and the smallest value is stored at one of its children
 import math
+import sys
 
 
 class MinMaxHeap:
 
     def __init__(self, array=None):
-        self.array = array if array is not None else []
+        self.__array = array if array is not None else []
+        for i in range(len(self.__array) // 2 + 1, -1, -1):
+            self.__pushDown(i)
+        for i in range(1, len(self.__array)):
+            self.__pushUp(i)
+
+    def removeAt(self, index: int):
+        if index == len(self.__array) - 1:
+            self.__array.pop()
+        else:
+            self.__swap(index, len(self.__array) - 1)
+            self.__pushDown(index)
+
+    def __pushDown(self, i: int):
+        if self.__isOnMinLevel(i):
+            self.__pushDownMin(i)
+        else:
+            self.__pushDownMax(i)
+
+    def __pushDownMin(self, i: int):
+        if not self.__hasChildren(i):
+            return
+        smallestChildOrGrandChild = self.__findSmallestChildOrGrandChild(i)
+        if self.__areParentAndChild(i, smallestChildOrGrandChild):
+            if self.__array[smallestChildOrGrandChild] < self.__array[i]:
+                self.__swap(i, smallestChildOrGrandChild)
+        else:
+            if self.__array[smallestChildOrGrandChild] < self.__array[i]:
+                self.__swap(i, smallestChildOrGrandChild)
+                smallestGrandchildParent = self.__parent(smallestChildOrGrandChild)
+                if self.__array[smallestChildOrGrandChild] > self.__array[smallestGrandchildParent]:
+                    self.__swap(smallestChildOrGrandChild, smallestGrandchildParent)
+        self.__pushDown(smallestChildOrGrandChild)
+
+    def __pushDownMax(self, i: int):
+        if not self.__hasChildren(i):
+            return
+        smallestChildOrGrandChild = self.__findSmallestChildOrGrandChild(i)
+        if self.__areParentAndChild(i, smallestChildOrGrandChild):
+            if self.__array[smallestChildOrGrandChild] > self.__array[i]:
+                self.__swap(smallestChildOrGrandChild, i)
+        else:
+            if self.__array[smallestChildOrGrandChild] > self.__array[i]:
+                self.__swap(smallestChildOrGrandChild, i)
+                smallestGrandChildParent = self.__parent(smallestChildOrGrandChild)
+                if self.__array[smallestChildOrGrandChild] < self.__array[smallestGrandChildParent]:
+                    self.__swap(smallestChildOrGrandChild, smallestGrandChildParent)
+        self.__pushDown(smallestChildOrGrandChild)
+
+    def __hasChildren(self, i):
+        return self.__firstChild(i) < len(self.__array)
+
+    def __findSmallestChildOrGrandChild(self, i):
+        return min(self.__firstChild(i),
+                   self.__secondChild(i),
+                   self.__firstChild(self.__firstChild(i)),
+                   self.__secondChild(self.__firstChild(i)),
+                   self.__firstChild(self.__secondChild(i)),
+                   self.__secondChild(self.__secondChild(i)),
+                   key=lambda x: self.__array[x] if x < len(self.__array) else sys.maxsize)
+
+    @staticmethod
+    def __firstChild(i: int) -> int:
+        return 2*i + 1
+
+    @staticmethod
+    def __secondChild(i: int):
+        return 2*i + 2
+
+    def __areParentAndChild(self, parent, child):
+        return self.__parent(child) == parent
 
     def add(self, element: int):
-        self.array.append(element)
-        self.__pushUp(len(self.array) - 1)
+        self.__array.append(element)
+        self.__pushUp(len(self.__array) - 1)
 
     def __pushUp(self, i):
         if i == 0:
@@ -35,14 +106,14 @@ class MinMaxHeap:
 
         parent = self.__parent(i)
         if self.__isOnMinLevel(i):
-            if self.array[i] > self.array[parent]:
-                self.array[i], self.array[parent] = self.array[parent], self.array[i]
+            if self.__array[i] > self.__array[parent]:
+                self.__swap(parent, i)
                 self.__pushUpMax(parent)
             else:
                 self.__pushUpMin(i)
         else:
-            if self.array[i] < self.array[parent]:
-                self.array[i], self.array[parent] = self.array[parent], self.array[i]
+            if self.__array[i] < self.__array[parent]:
+                self.__swap(parent, i)
                 self.__pushUpMin(parent)
             else:
                 self.__pushUpMax(i)
@@ -55,33 +126,33 @@ class MinMaxHeap:
 
     def __pushUpMax(self, i: int):
         grandParent = self.__parent(self.__parent(i))
-        if grandParent >= 0 and self.array[i] > self.array[grandParent]:
-            self.array[i], self.array[grandParent] = self.array[grandParent], self.array[i]
+        if grandParent >= 0 and self.__array[i] > self.__array[grandParent]:
+            self.__swap(grandParent, i)
             self.__pushUpMax(grandParent)
 
     def __pushUpMin(self, i: int):
         grandParent = self.__parent(self.__parent(i))
-        if grandParent >= 0 and self.array[i] < self.array[grandParent]:
-            self.array[i], self.array[grandParent] = self.array[grandParent], self.array[i]
+        if grandParent >= 0 and self.__array[i] < self.__array[grandParent]:
+            self.__swap(grandParent, i)
             self.__pushUpMin(grandParent)
 
     @staticmethod
     def __parent(i: int) -> int:
         return (i + 1) // 2 - 1
 
+    def __swap(self, i, smallestChildOrGrandChild):
+        self.__array[smallestChildOrGrandChild], self.__array[i] = self.__array[i], self.__array[smallestChildOrGrandChild]
+
     def min(self):
-        if not self.array:
+        if not self.__array:
             raise AssertionError("Tried to call min on an empty min-max heap")
-        return self.array[0]
+        return self.__array[0]
 
     def max(self):
-        if not self.array:
+        if not self.__array:
             raise AssertionError("Tried to call max on an empty min-max heap")
-        if len(self.array) == 1:
-            return self.array[0]
-        if len(self.array) == 2:
-            return self.array[1]
-        return max(self.array[1], self.array[2])
-
-    def remove(self, element: int):
-        pass
+        if len(self.__array) == 1:
+            return self.__array[0]
+        if len(self.__array) == 2:
+            return self.__array[1]
+        return max(self.__array[1], self.__array[2])
--- test/min_max_heap/heap_test.py
+++ test/min_max_heap/heap_test.py
@@ -10,13 +10,20 @@ class MinMaxHeapTest(unittest.TestCase):
         heap.add(10)
         heap.add(15)
         heap.add(7)
+        heap.removeAt(2)
         heap.add(2)
         heap.add(22)
         heap.add(100)
+        heap.removeAt(4)
         heap.add(0)
         heap.add(1)
+        heap.removeAt(5)
 
         self.assertEqual(0, heap.min())
         self.assertEqual(100, heap.max())
 
+    def test_initiallyNotEmptyHeap(self):
+        heap = MinMaxHeap([10, 15, 7, 2, 22, 100, 0, 1])
 
+        self.assertEqual(0, heap.min())
+        self.assertEqual(100, heap.max())
